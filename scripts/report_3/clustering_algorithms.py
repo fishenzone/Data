a8762980923a20c8d1954a5db6e5a34abd6ce825
fishenzone <simonfrizen98@gmail.com>
2024-01-22 22:46:36 +0600

    Fix clustering_algo.py

--- scripts/report_3/clustering_algorithms.py
+++ scripts/report_3/clustering_algorithms.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 import hdbscan
-from sklearn.cluster import AgglomerativeClustering, DBSCAN, SpectralClustering, MeanShift, AffinityPropagation, OPTICS, Birch
+from sklearn.cluster import AgglomerativeClustering, DBSCAN, KMeans, SpectralClustering, MeanShift, AffinityPropagation, OPTICS, Birch
 
 from .tf_idf_pipeline import calculate_clustering_metrics
 
@@ -15,7 +15,8 @@ def perform_all_clustering(X, n_clusters, algorithm):
         'AffinityPropagation': AffinityPropagation(),
         'HDBSCAN': hdbscan.HDBSCAN(min_cluster_size=10),
         'OPTICS': OPTICS(min_samples=10),
-        'BIRCH': Birch(n_clusters=n_clusters)
+        'BIRCH': Birch(n_clusters=n_clusters),
+        'KMeans': KMeans(n_clusters=n_clusters, n_init=10)
     }
 
     try:
@@ -30,8 +31,22 @@ def calculate_all_metrics(X, n_clusters, ds, all_labels):
     df = ds.copy()
     all_metrics = []
     for algorithm, labels in all_labels.items():
-        metrics, df = calculate_clustering_metrics(X, n_clusters, df, labels)
-        metrics['algorithm'] = algorithm
+        try:
+            metrics, df = calculate_clustering_metrics(X, n_clusters, df, labels)
+            metrics['algorithm'] = algorithm
+        except Exception as e:
+            print(f"An error occurred with {algorithm}: {str(e)}")
+            metrics = pd.DataFrame({
+                'algorithm': [algorithm],
+                'N clusters': [0],
+                'SS': [0],
+                'ARI': [0],
+                'AMI': [0],
+                'Equal': [False],
+                'Acc': [0]
+            })
         all_metrics.append(metrics)
     all_metrics_df = pd.concat(all_metrics, ignore_index=True)
     return all_metrics_df, df
+
+
