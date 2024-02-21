1cf1c3a37b3d6ebf2d8402b174e071e0fb985ed5
fishenzone <simonfrizen98@gmail.com>
2024-02-21 16:31:18 +0600

    Add pred_labels

--- scripts/report_2/wbs.py
+++ scripts/report_2/wbs.py
@@ -5,3 +5,7 @@ decoder = build_ctcdecoder(
     alpha=0.02,  # tuned on a val set
     beta=.5,  # tuned on a val set
 )
+
+from tqdm import tqdm
+all_pred_labels_wbs = [decoder.decode(prob, beam_width=300) for prob in tqdm(
+    all_pred_probs, desc=f"Decoding with {kenlm_model_path}")]
