6d495bd0e49ae2e712933e70a4ec815c29291907
fishenzone <simonfrizen98@gmail.com>
2024-03-06 00:15:17 +0600

    Update func

--- scripts/report_3/word2vec_pipeline.py
+++ scripts/report_3/word2vec_pipeline.py
@@ -1,25 +1,5 @@
-import os
-import re
-import glob
-
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-import nltk
-from nltk.corpus import stopwords
 from gensim.models import Word2Vec
-
-from sklearn.manifold import TSNE
-from sklearn.cluster import KMeans
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import silhouette_score, adjusted_rand_score, adjusted_mutual_info_score
-
-from umap import UMAP
-
-# cluster_to_doctype = df.groupby('cluster')['doctype'].agg(lambda x: x.value_counts().index[0]).to_dict()
 
 def split_text_into_tokens(df):
     df['text_tokenized'] = df['text_processed'].apply(lambda x: x.split())
